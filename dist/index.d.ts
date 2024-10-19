a7f22780733a9fd70457cbd56b2637611f30e517
Emir Kabal <kabalemir@gmail.com>
2024-10-19 03:32:26 +0300

    init

---
+++ dist/index.d.ts
@@ -0,0 +1,14 @@
+import {
+  KrispNoiseFilterProcessor,
+  type NoiseFilterOptions,
+} from "./NoiseFilterProcessor";
+declare const isKrispNoiseFilterSupported: typeof KrispNoiseFilterProcessor.isSupported;
+declare const KrispNoiseFilter: (
+  options?: NoiseFilterOptions
+) => KrispNoiseFilterProcessor;
+export {
+  isKrispNoiseFilterSupported,
+  KrispNoiseFilter,
+  NoiseFilterOptions,
+  type KrispNoiseFilterProcessor,
+};
